f040d4fe881b617ff0bc3f6da9a45fb83ad449f9
PAVITHRA-A2 <pavithra15072005vi@gmail.com>
2025-02-24 10:09:20 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { QRcode } from './component.jsx/QRcode'
+import './QRcode.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <QRcode />
+    </>
+  )
+}
+
+export default App
